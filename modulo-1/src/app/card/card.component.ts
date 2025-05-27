5c39194ecb90635ef243dc0c2e6db9225b382424
carlos.brandao <carlos.brandao@fpf.br>
2025-05-27 12:07:48 -0400

    viewencapsulation

--- modulo-1/src/app/card/card.component.ts
+++ modulo-1/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 interface IPlano {
   infos: IInfos;
@@ -12,7 +12,8 @@ interface IInfos {
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CardComponent {
   //@ts-ignore
